93184b358c68c585f842a2739a3bec21aef2dd67
Ramaswamy <srramas@acbc32c3dd6d.ant.amazon.com>
2018-11-17 21:38:47 +1100

    initial design

---
+++ src/main/java/com/awssupport/athena/web/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.awssupport.athena.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/excecises").setViewName("excecises");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/error").setViewName("error");
+
+
+    }
+
+
+}
